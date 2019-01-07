50560fe7d9862b7d08c626142e34dccb8bfc5add
Barry Qin <cswords@users.noreply.github.com>
2019-01-07 18:36:18 +1100

    firebase deployment

--- src/js/config/development.config.js
+++ src/js/config/development.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    kajeroHomepage: '/',
+    kajeroHomepage: 'https://github.com/cswords/kajero',
     gistUrl: 'https://gist.githubusercontent.com/anonymous/',
     gistApi: 'https://api.github.com/gists',
     cssUrl: 'dist/main.css',
--- src/js/config/production.config.js
+++ src/js/config/production.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    kajeroHomepage: 'http://www.joelotter.com/kajero/',
+    kajeroHomepage: 'https://github.com/cswords/kajero',
     gistUrl: 'https://gist.githubusercontent.com/anonymous/',
     gistApi: 'https://api.github.com/gists',
-    cssUrl: 'http://www.joelotter.com/kajero/dist/main.css',
-    scriptUrl: 'http://www.joelotter.com/kajero/dist/bundle.js'
+    cssUrl: 'https://js.foolish.ai/dist/main.css',
+    scriptUrl: 'https://js.foolish.ai/dist/bundle.js'
 }
